0c493307df230fa210ca184ffb8d9536e5db969b
ovi_daescu <ovi.daescu8@gmail.com>
2025-05-23 22:07:50 +0300

    sequlize

--- models/index.js
+++ models/index.js
@@ -2,15 +2,29 @@
 
 const Sequelize = require('sequelize');
 const process = require('process');
-const config = require(__dirname + '/../config/config.json')[process.env.NODE_ENV || 'production'];
+const env = process.env.NODE_ENV || 'production';
+const config = require(__dirname + '/../config/config.json')[env];
 const db = {};
+
+// Use environment variables if available, otherwise fallback to config.json
+const dbHost = process.env.DB_HOST || config.host;
+const dbPort = process.env.DB_PORT || config.port || 5432;
+const dbUser = process.env.DB_USER || config.username;
+const dbPassword = process.env.DB_PASSWORD || config.password;
+const dbName = process.env.DB_NAME || config.database;
 
 let sequelize;
 if (config.use_env_variable) {
   sequelize = new Sequelize(process.env[config.use_env_variable], config);
 } else {
-  sequelize = new Sequelize(config.database, config.username, config.password, config);
+  sequelize = new Sequelize(dbName, dbUser, dbPassword, {
+    ...config,
+    host: dbHost,
+    port: dbPort,
+  });
 }
+
+// ...rest of your code...
 
 // Directly import models
 const Car = require('./car')(sequelize, Sequelize.DataTypes);
