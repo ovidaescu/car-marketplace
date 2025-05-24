2ca7e80544af75d429cbd3467feddad94e3f18c6
ovi_daescu <ovi.daescu8@gmail.com>
2025-05-24 19:28:28 +0300

    revertSocket

--- utils/start.js
+++ utils/start.js
@@ -1,7 +1,6 @@
 const { startWebSocketServer } = require('./websocket');
 
-(async () => {
-  // Start the WebSocket server on port 8081 and get the broadcastUpdate function
-  const broadcastUpdate = await startWebSocketServer(8081);
-  module.exports = { broadcastUpdate };
-})();
+// Start the WebSocket server on port 8080 and get the broadcastUpdate function
+const broadcastUpdate = startWebSocketServer(8081);
+
+module.exports = { broadcastUpdate };
