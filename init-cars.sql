f7f1825944351e2e0d3e26f63227fcb46d8e13cb
ovi_daescu <ovi.daescu8@gmail.com>
2025-05-24 18:23:49 +0300

    DB

---
+++ init-cars.sql
@@ -0,0 +1,7 @@
+INSERT INTO cars (make, model, type, year, km, fuel, price, dateAdded, photoUrl, ownerId)
+VALUES
+  ('Toyota', 'Corolla', 'Hatchback', 2020, 10000, 'Petrol', 18000, '2025-03-16', 'https://media.istockphoto.com/id/1490889104/photo/toyota-corolla-hybrid.jpg?s=612x612&w=0&k=20&c=_9l8ZyisQAh86uwKuyCWOZossiapILIYsR_5Xa-tSDY=',1),
+  ('Honda', 'Civic', 'Sedan', 2021, 65000, 'Diesel', 23000, '2025-03-13', 'https://www.topgear.com/sites/default/files/images/news-article/2020/01/b905025387c82f922928e39fb3e9357c/2020-honda-civic-type-r1.jpg',2),
+  ('Ford', 'Mustang', 'Coupe', 2019, 30000, 'Petrol', 28000, '2025-02-12', 'https://www.topgear.com/sites/default/files/cars-car/image/2024/12/mustang_lightning_blue_009.jpg',1),
+  ('BMW', 'X5', 'SUV', 2015, 150000, 'Diesel', 22000, '2025-02-07', 'https://media.ed.edmunds-media.com/bmw/x5-m/2015/oem/2015_bmw_x5-m_4dr-suv_base_fq_oem_1_1600.jpg',3),
+  ('Audi', 'A4', 'Break', 2020, 41000, 'Petrol', 24000, '2025-01-11', 'https://car-images.bauersecure.com/wp-images/12793/audi_a4_100.jpg',4);
